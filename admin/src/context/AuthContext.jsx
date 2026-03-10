d3179e7629f3e89dd5cc42955479be03ca3480b6
Arihant\prince kushwah <prince.Kushwah@ARIHANT.CO.IN>
2026-03-10 18:16:56 +0530

    Frontend: update backend URL for production

--- admin/src/context/AuthContext.jsx
+++ admin/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 
 export const authDataContext = createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:5000"
+    let serverUrl = "https://docart-e-commerce.onrender.com"
 
     let value = {
       serverUrl
--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createContext } from 'react'
 export const authDataContext= createContext()
 function AuthContext({children}) {
-    let serverUrl = "http://localhost:5000"
+    let serverUrl = "https://docart-e-commerce.onrender.com"
 
     let value = {
        serverUrl
